ca31e95a4ffd0cde34c4877061fde55f6816c605
Virgile Bello <virgile@siliconstudio.co.jp>
2016-09-21 18:57:53 +0900

    Add image captions automatically, and align lists properly

---
+++ template/styles/main.js
@@ -0,0 +1,9 @@
+$(function() {
+  function showCaptionFromAlt(selector){
+    $(selector).each(function() {
+      $(this).after( '<span class="img-caption">' + $(this).attr('alt') + '</span>' );
+    });
+  };
+  
+  showCaptionFromAlt("article img");
+});
